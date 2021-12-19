994fb3311fa80da69f68cddafbf899f10d11bcfa
JosieMCantu <josiex2@gmail.com>
2021-12-18 21:01:44 -0800

    inital commit

---
+++ src/app/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className='main-container'>
+      <div className='header-container'>
+        <div className='header'>
+        ✻ This site is under construction. ✻
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <div>
+            <ul>
+                <li>LINK</li>
+                <li>LINK</li>
+                <li>LINK</li>
+                <li>LINK</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar;
